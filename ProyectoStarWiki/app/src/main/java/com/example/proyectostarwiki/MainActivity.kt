15f0d98a7152ca70b17ad7a3e3b39973114ec723
LuisGarrigosa <garri01yt@gmail.com>
2023-04-11 13:53:52 +0200

    Login terminado

--- ProyectoStarWiki/app/src/main/java/com/example/proyectostarwiki/MainActivity.kt
+++ ProyectoStarWiki/app/src/main/java/com/example/proyectostarwiki/MainActivity.kt
@@ -1,11 +1,79 @@
 package com.example.proyectostarwiki
 
+import android.content.Intent
+import android.content.res.Resources.Theme
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.proyectostarwiki.databinding.ActivityMainBinding
+import com.example.proyectostarwiki.prefs.Prefs
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 class MainActivity : AppCompatActivity() {
+
+        lateinit var binding: ActivityMainBinding
+        lateinit var prefs: Prefs
+
+        private val responseLauncher= registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            if (it.resultCode== RESULT_OK){
+                val task = GoogleSignIn.getSignedInAccountFromIntent(it.data)
+                try{
+                    val cuenta = task.getResult(ApiException::class.java)
+                    if (cuenta!=null){
+                        val credenciales = GoogleAuthProvider.getCredential(cuenta.idToken, null)
+                        FirebaseAuth.getInstance().signInWithCredential(credenciales).addOnCompleteListener {
+                            if (it.isSuccessful){
+                                prefs.setEmail(cuenta.email?:"")
+                                irMenu()
+                            } else {
+                                println(it.result.toString())
+                            }
+                        }
+                    }
+                }catch (e:ApiException){
+                    println(e.message)
+                }
+            }
+        }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        prefs= Prefs(this)
+        comprobarLogin()
+        setListeners()
+    }
+
+    private fun irMenu() {
+        startActivity(Intent(this,MenuActivity::class.java))
+    }
+
+    private fun setListeners() {
+        binding.botonGoogle.setOnClickListener {
+            login()
+        }
+    }
+
+    private fun login() {
+        val googleConf = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("902621700012-kl0fioie77dgdjormue63u09ggtpash9.apps.googleusercontent.com")
+            .requestEmail()
+            .build()
+        val googleClient = GoogleSignIn.getClient(this, googleConf)
+
+        googleClient.signOut()
+
+        responseLauncher.launch(googleClient.signInIntent)
+    }
+
+    private fun comprobarLogin() {
+        val email = prefs.getEmail()
+        if (email!=null){
+            irMenu()
+        }
     }
 }
--- ProyectoStarWiki/app/src/main/java/com/example/proyectostarwiki/MenuActivity.kt
+++ ProyectoStarWiki/app/src/main/java/com/example/proyectostarwiki/MenuActivity.kt
@@ -1,11 +1,54 @@
 package com.example.proyectostarwiki
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.example.proyectostarwiki.databinding.ActivityMenuBinding
+import com.example.proyectostarwiki.prefs.Prefs
+import com.google.firebase.auth.FirebaseAuth
 
 class MenuActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMenuBinding
+    lateinit var prefs:Prefs
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_menu)
+        binding = ActivityMenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        prefs= Prefs(this)
+        setListeners()
+    }
+
+    private fun setListeners() {
+        binding.btnNaves.setOnClickListener{
+            irNaves()
+        }
+    }
+
+    private fun irNaves() {
+        startActivity(Intent(this,NavesActivity::class.java))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.itemSesion->{
+                FirebaseAuth.getInstance().signOut()
+                prefs.borrarTodo()
+                finish()
+            }
+
+            R.id.itemSalir->{
+                finishAffinity()
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
